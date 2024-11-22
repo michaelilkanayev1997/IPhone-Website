ea8b4cdae02b69c67761acaaa286d27f016d1076
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-11-22 20:49:10 +0200

    Added HowItWorks & Footer Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
-
 import * as Sentry from "@sentry/react";
+
 import Features from "./components/Features";
+import Footer from "./components/Footer";
+import HowItWorks from "./components/HowItWorks";
+
 const App = () => {
   return (
     <main className="bg-black">
@@ -13,6 +16,8 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 };
